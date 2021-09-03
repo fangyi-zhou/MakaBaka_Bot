71044cf8f1b4f95341d750490bdc26cd636845a6
Fangyi Zhou <me@fangyi.io>
2021-09-03 13:16:34 +0100

    Add goodnight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from bot_token import TOKEN
 
@@ -16,7 +16,14 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="玛卡巴卡")
 
 
+def goodnight(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="晚安玛卡巴卡")
+
 start_handler = CommandHandler("start", start)
+goodnight_handler = MessageHandler(Filters.regex("^晚安.*") |
+                                   Filters.regex(".*晚安$"), goodnight)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(goodnight_handler)
 
 updater.start_polling()
